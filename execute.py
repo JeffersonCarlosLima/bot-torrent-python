00b3eba229fdfd62fb17f93f552c9c7b11419021
JeffersonCarlosLima <jeffersoncl200@gmail.com>
2024-07-15 20:45:21 -0300

    commit

--- execute.py
+++ execute.py
@@ -4,17 +4,20 @@ import subprocess
 
 def execute_script():
     # Aqui você pode adicionar o caminho para o script ou programa que deseja executar
-    script_path = "C:\\Users\\Marketing\\Documents\\Projetos\\executar programas\\bot.py"  # Por exemplo, um script bash
+    script_path = "C:\\Users\\jeffe\\AppData\\Local\\Programs\\TikTok Music\\TikTok Music Launcher.exe"
     try:
-        subprocess.run(["/bin/bash", script_path], check=True)
+        subprocess.run([script_path], check=True)
         print("Script executado com sucesso.")
     except subprocess.CalledProcessError as e:
         print(f"Erro ao executar o script: {e}")
 
 # Agendar a execução do script a cada 4 horas
-schedule.every(1).minutes.do(execute_script)
+schedule.every(4).hours.do(execute_script)
 
 # Loop infinito para manter o programa rodando
 while True:
     schedule.run_pending()
     time.sleep(1)
+#python -m venv projTor
+#projTor\Scripts\activate
+#deactivate  -- destaiva o venv
